ebed48a415c61992ee304d502d57859bbf59427d
Gabriel Ferreira <gabrielferreira@Gabriels-MacBook-Air.local>
2024-07-06 17:47:45 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # Permite requisições cross-origin (CORS)
+
+# Exemplo de rota para receber uma requisição GET
+@app.route('/api/data', methods=['GET'])
+def get_data():
+    data = {'message': 'Hello from Flask!'}
+    return jsonify(data)
+
+if __name__ == '__main__':
+    app.run(debug=True)
